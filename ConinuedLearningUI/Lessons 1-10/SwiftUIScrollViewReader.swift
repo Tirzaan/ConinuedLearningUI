01f62190f9a1887336b44dab78863bfc7b230529
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 12:07:57 -0600

    [Feature] Added Button "Go To 49" in Scroll View Reader

--- ConinuedLearningUI/Lessons 1-10/SwiftUIScrollViewReader.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIScrollViewReader.swift
@@ -12,6 +12,11 @@ struct SwiftUIScrollViewReader: View {
     var body: some View {
         ScrollView {
             ScrollViewReader { proxy in
+                
+                Button("Go To #49") {
+                    proxy.scrollTo(49, anchor: nil)
+                }
+                
                 ForEach(0..<50) { index in
                     Text("This is item #\(index)")
                         .font(.headline)
@@ -21,6 +26,7 @@ struct SwiftUIScrollViewReader: View {
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                         .shadow(radius: 10)
                         .padding()
+                        .id(index)
                 }
             }
         }
