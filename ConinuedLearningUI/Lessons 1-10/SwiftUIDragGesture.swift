d6ec98b4befa43ee61eca5bcdaa93033398464e8
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 07:49:17 -0600

    [Feature] Created Lesson 4 Drag Gesture

---
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIDragGesture.swift
@@ -0,0 +1,19 @@
+// Lesson 4
+//
+//  SwiftUIDragGesture.swift
+//  ConinuedLearningUI
+//
+//  Created by Tirzaan on 7/24/25.
+//
+
+import SwiftUI
+
+struct SwiftUIDragGesture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIDragGesture()
+}
