7268504eba1d57126972ab3f148a5c252469d4ae
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 15:21:25 -0600

    [Feature] Added Mask UI

--- ConinuedLearningUI/Lessons 1-10/SwiftUIMask.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIMask.swift
@@ -9,8 +9,49 @@
 import SwiftUI
 
 struct SwiftUIMask: View {
+    
+    @State var rating: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            starsView
+                .overlay { overlayView.mask(starsView) }
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .fill(
+                        LinearGradient(
+                            colors: [.red, .yellow],
+                            startPoint: .top,
+                            endPoint: .bottom)
+                    )
+                    .frame(width: CGFloat(rating) / 5  * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            if rating != index || index != 1 {
+                                rating = index
+                            } else {
+                                rating -= 1
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
