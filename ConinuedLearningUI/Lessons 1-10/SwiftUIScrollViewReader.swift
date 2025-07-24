3514b8503369b054c6ec9668710c2fa897964be7
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 12:04:01 -0600

    [Feature] Moved Scroll View Reader content into a ScrollViewReader

--- ConinuedLearningUI/Lessons 1-10/SwiftUIScrollViewReader.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIScrollViewReader.swift
@@ -11,15 +11,17 @@ import SwiftUI
 struct SwiftUIScrollViewReader: View {
     var body: some View {
         ScrollView {
-            ForEach(0..<50) { index in
-                Text("This is item #\(index)")
-                    .font(.headline)
-                    .frame(height: 200)
-                    .frame(maxWidth: .infinity)
-                    .background(.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .shadow(radius: 10)
-                    .padding()
+            ScrollViewReader { proxy in
+                ForEach(0..<50) { index in
+                    Text("This is item #\(index)")
+                        .font(.headline)
+                        .frame(height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .shadow(radius: 10)
+                        .padding()
+                }
             }
         }
     }
