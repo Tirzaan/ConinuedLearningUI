b19a238068cbe859c763d116a7c909292513364a
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 07:09:15 -0600

    [Feature] Added Magnification Gesture UI

--- ConinuedLearningUI/ConinuedLearningUIApp.swift
+++ ConinuedLearningUI/ConinuedLearningUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ConinuedLearningUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SwiftUIMagnificationGesture()
         }
     }
 }
--- ConinuedLearningUI/Lessons 1-10/SwiftUIMagnificationGesture.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIMagnificationGesture.swift
@@ -9,8 +9,27 @@
 import SwiftUI
 
 struct SwiftUIMagnificationGesture: View {
+    
+    @State var currentAmount: CGFloat = 0
+    @State var lastAmount: CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+            .font(.title)
+            .padding(40)
+            .background(.red)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .scaleEffect(1 + currentAmount + lastAmount)
+            .gesture(
+                MagnificationGesture()
+                    .onChanged { value in
+                        currentAmount = value - 1
+                    }
+                    .onEnded { value in
+                        lastAmount += currentAmount
+                        currentAmount = 0
+                    }
+            )
     }
 }
 
