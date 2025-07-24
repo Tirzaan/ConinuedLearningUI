77256de4a4523fb3a84d61fcf245bbcbee54c9be
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 15:59:17 -0600

    [Feature] Added Sound Effects UI

--- ConinuedLearningUI/Lessons 1-10/SwiftUISoundEffects.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUISoundEffects.swift
@@ -7,10 +7,44 @@
 //
 
 import SwiftUI
+import AVKit
+
+/// Manager for all Sound Playing
+class SoundManager {
+    static let instance = SoundManager() // Singleton
+    
+    var player: AVAudioPlayer?
+    
+    enum soundOption: String {
+        case tada
+        case badumtss
+    }
+    
+    /// Plays a selected sound from the soundOptions enum
+    func playSound(sound: soundOption) {
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+        
+    }
+}
 
 struct SwiftUISoundEffects: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            Button("Play Sound 1") {
+                SoundManager.instance.playSound(sound: .tada)
+            }
+            
+            Button("Play Sound 2") {
+                SoundManager.instance.playSound(sound: .badumtss)
+            }
+        }
     }
 }
 
