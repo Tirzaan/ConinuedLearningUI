f6ac8770275990e4eb50269fe2bcec0dd5001ea2
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 07:41:11 -0600

    [Feature] Added Rotation Gesture UI

--- ConinuedLearningUI/Lessons 1-10/SwiftUIRotationGesture.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIRotationGesture.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct SwiftUIRotationGesture: View {
+    
+    @State var angle: Angle = Angle(degrees: 0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundStyle(.white, .black)
+            .padding(50)
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged { value in
+                        angle = value
+                    }
+                    .onEnded { value in
+                        withAnimation(.spring){
+                            angle = Angle(degrees: 0)
+                        }
+                    }
+            )
     }
 }
 
