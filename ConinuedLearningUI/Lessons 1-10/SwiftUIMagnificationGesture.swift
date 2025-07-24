5112cd9b08f3478548ef787261b0e51450d17edb
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 07:25:15 -0600

    [Feature] Changed Magnification Gesture UI

--- ConinuedLearningUI/Lessons 1-10/SwiftUIMagnificationGesture.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIMagnificationGesture.swift
@@ -14,22 +14,61 @@ struct SwiftUIMagnificationGesture: View {
     @State var lastAmount: CGFloat = 0
     
     var body: some View {
-        Text("Hello, World!")
-            .font(.title)
-            .padding(40)
-            .background(.red)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .scaleEffect(1 + currentAmount + lastAmount)
-            .gesture(
-                MagnificationGesture()
-                    .onChanged { value in
-                        currentAmount = value - 1
-                    }
-                    .onEnded { value in
-                        lastAmount += currentAmount
-                        currentAmount = 0
-                    }
-            )
+        VStack(spacing: 10) {
+            HStack {
+                Circle()
+                    .frame(width: 35, height: 35)
+                Text("Swiftful Thinking")
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .padding(.horizontal)
+            
+            Rectangle()
+                .frame(height: 300)
+                .scaleEffect(1 + currentAmount)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { value in
+                            currentAmount = value - 1
+                        }
+                        .onEnded { value in
+                            withAnimation(.spring) {
+                                currentAmount = 0
+                            }
+                        }
+                )
+            
+            HStack {
+                Image(systemName: "heart.fill")
+                Image(systemName: "text.bubble.fill")
+                Spacer()
+            }
+            .padding(.horizontal)
+            .font(.headline)
+            
+            Text("This the the caption for my post")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+        }
+        
+        
+//        Text("Hello, World!")
+//            .font(.title)
+//            .padding(40)
+//            .background(.red)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .scaleEffect(1 + currentAmount + lastAmount)
+//            .gesture(
+//                MagnificationGesture()
+//                    .onChanged { value in
+//                        currentAmount = value - 1
+//                    }
+//                    .onEnded { value in
+//                        lastAmount += currentAmount
+//                        currentAmount = 0
+//                    }
+//            )
     }
 }
 
