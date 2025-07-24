b038e190d9b9553a98cb81ef2f112144689b02cd
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 06:18:18 -0600

    Initial Commit

---
+++ ConinuedLearningUI/ConinuedLearningUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConinuedLearningUIApp.swift
+//  ConinuedLearningUI
+//
+//  Created by Tirzaan on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ConinuedLearningUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
