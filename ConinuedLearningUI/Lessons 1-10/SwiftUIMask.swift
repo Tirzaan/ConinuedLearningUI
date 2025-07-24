1a92c6cee74974892ecc85a721c39adfbc308491
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 14:45:55 -0600

    [Created] Lesson 8 Mask

---
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIMask.swift
@@ -0,0 +1,19 @@
+// Lesson 8
+//
+//  SwiftUIMask.swift
+//  ConinuedLearningUI
+//
+//  Created by Tirzaan on 7/24/25.
+//
+
+import SwiftUI
+
+struct SwiftUIMask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIMask()
+}
