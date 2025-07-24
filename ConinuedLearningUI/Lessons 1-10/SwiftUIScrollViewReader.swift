8af0f9c98600a5bf763f42acbcb323720b9fae00
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 12:31:28 -0600

    [Feature] Finished Scroll View Reader UI

--- ConinuedLearningUI/Lessons 1-10/SwiftUIScrollViewReader.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIScrollViewReader.swift
@@ -9,24 +9,43 @@
 import SwiftUI
 
 struct SwiftUIScrollViewReader: View {
+    
+    @State var textFieldValue: String = ""
+    @State var scrollToIndex: Int = 0
+    
     var body: some View {
-        ScrollView {
-            ScrollViewReader { proxy in
-                
-                Button("Go To #49") {
-                    proxy.scrollTo(49, anchor: nil)
+        VStack {
+            TextField("Enter a number here", text: $textFieldValue)
+                .frame(height: 55)
+                .padding(.leading)
+                .border(.gray)
+                .padding()
+                .keyboardType(.numberPad)
+            
+            Button("Scroll To \(textFieldValue)") {
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    let index = Int(textFieldValue) ?? 0
+                    scrollToIndex = index
+//                    proxy.scrollTo(30, anchor: .center)
                 }
-                
-                ForEach(0..<50) { index in
-                    Text("This is item #\(index)")
-                        .font(.headline)
-                        .frame(height: 200)
-                        .frame(maxWidth: .infinity)
-                        .background(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .shadow(radius: 10)
-                        .padding()
-                        .id(index)
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index)")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                    }
+                    .onChange(of: scrollToIndex) { oldValue, newValue in
+                        proxy.scrollTo(newValue, anchor: .top)
+                    }
                 }
             }
         }
