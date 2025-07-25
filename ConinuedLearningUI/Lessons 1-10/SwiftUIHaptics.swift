94aec485cee85c93afd28ad939d48f9ba2e04741
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 19:30:36 -0600

    [Feature] Created Haptics

---
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIHaptics.swift
@@ -0,0 +1,19 @@
+// Lesson 10
+//
+//  SwiftUIHaptics.swift
+//  ConinuedLearningUI
+//
+//  Created by Tirzaan on 7/24/25.
+//
+
+import SwiftUI
+
+struct SwiftUIHaptics: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIHaptics()
+}
