373cc954db90bbce99d9d1244742f457b2a77c08
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-25 11:30:39 -0600

    [Feature] Created Hashable and UI

--- ConinuedLearningUI/ContentView.swift
+++ ConinuedLearningUI/ContentView.swift
@@ -23,7 +23,7 @@ struct ContentView: View {
                 NavigationLink("9 - Sound Effects") { SwiftUISoundEffects() }
                 NavigationLink("10 - Haptics") { SwiftUIHaptics() }
                 NavigationLink("11 - Local Notifications") { SwiftUILocalNotifications() }
-                NavigationLink("12 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
+                NavigationLink("12 - Hashable") { SwiftUIHashable() }
                 NavigationLink("13 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
                 NavigationLink("14 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
                 NavigationLink("15 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
---
+++ ConinuedLearningUI/Lessons 11-20/SwiftUIHashable.swift
@@ -0,0 +1,45 @@
+// Lesson 12
+//
+//  SwiftUIHashable.swift
+//  ConinuedLearningUI
+//
+//  Created by Tirzaan on 7/25/25.
+//
+
+import SwiftUI
+
+struct MyCustomModel: Hashable {
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+    
+}
+
+struct SwiftUIHashable: View {
+    
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "One"),
+        MyCustomModel(title: "Two"),
+        MyCustomModel(title: "Three"),
+        MyCustomModel(title: "Four"),
+        MyCustomModel(title: "Five")
+    ]
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(data, id: \.self) { item in
+                    Text(item.hashValue.description)
+                        .font(.headline)
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    SwiftUIHashable()
+}
