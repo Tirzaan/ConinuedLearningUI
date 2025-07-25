40dd245bbaee6a0a9a8c349ea2a2c140aa533b6c
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-24 19:43:00 -0600

    [Feature] Added Haptic UI

--- ConinuedLearningUI/Lessons 1-10/SwiftUIHaptics.swift
+++ ConinuedLearningUI/Lessons 1-10/SwiftUIHaptics.swift
@@ -8,9 +8,40 @@
 
 import SwiftUI
 
+class HapticManager {
+    
+    static let instance = HapticManager() //Singletion
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+    
+}
+
 struct SwiftUIHaptics: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(spacing: 20) {
+                Button("notification success") { HapticManager.instance.notification(type: .success) }
+                Button("notification warning") { HapticManager.instance.notification(type: .warning) }
+                Button("notification error") { HapticManager.instance.notification(type: .error) }
+            }
+            
+            VStack(spacing: 20) {
+                Button("impact soft") { HapticManager.instance.impact(style: .soft) }
+                Button("impact light") { HapticManager.instance.impact(style: .light) }
+                Button("impact medium") { HapticManager.instance.impact(style: .medium) }
+                Button("impact rigid") { HapticManager.instance.impact(style: .rigid) }
+                Button("impact heavy") { HapticManager.instance.impact(style: .heavy) }
+            }
+
+        }
     }
 }
 
