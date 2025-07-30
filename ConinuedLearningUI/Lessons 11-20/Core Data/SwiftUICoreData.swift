235773a47e150cc4ddc8a3363e52f6985a18d101
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-29 21:53:04 -0600

    [Feature] Updated Core Data UI

--- ConinuedLearningUI/Lessons 11-20/Core Data/SwiftUICoreData.swift
+++ ConinuedLearningUI/Lessons 11-20/Core Data/SwiftUICoreData.swift
@@ -40,6 +40,20 @@ class CoreDataViewModel: ObservableObject {
         saveData()
     }
     
+    func updateFruit(entity: FruitEntity) {
+        let currentName = entity.name ?? ""
+        let newName = currentName + "!"
+        entity.name = newName
+        saveData()
+    }
+    
+    func deleteFruit(indexSet: IndexSet) {
+        guard let index = indexSet.first else { return }
+        let entity = savedEntities[index]
+        container.viewContext.delete(entity)
+        saveData()
+    }
+    
     func saveData() {
         do {
             try container.viewContext.save()
@@ -55,9 +69,47 @@ class CoreDataViewModel: ObservableObject {
 struct SwiftUICoreData: View {
     
     @StateObject var viewModel = CoreDataViewModel()
+    @State var AddFruitText: String = ""
     
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            VStack(spacing: 20) {
+                TextField("Add Fruit here", text: $AddFruitText)
+                    .font(.headline)
+                    .padding()
+                    .padding(.horizontal, -5)
+                    .background(.gray.opacity(0.25))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal)
+                
+                Button {
+                    guard !AddFruitText.isEmpty else { return }
+                    viewModel.addFruit(text: AddFruitText)
+                    AddFruitText = ""
+                } label: {
+                    Text("Save")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .padding(.horizontal)
+                }
+
+                List {
+                    ForEach(viewModel.savedEntities) { entity in
+                        Text(entity.name ?? "No Name")
+                            .onTapGesture {
+                                viewModel.updateFruit(entity: entity)
+                            }
+                    }
+                    .onDelete(perform: viewModel.deleteFruit)
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle(Text("Fruits"))
+        }
     }
 }
 
