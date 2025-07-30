13538f3bafdc283b12127d1f7f37241cc941cc20
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-29 20:54:05 -0600

    [Feature] Created Core Data and Fruits Contaner

---
+++ ConinuedLearningUI/Lessons 11-20/Core Data/SwiftUICoreData.swift
@@ -0,0 +1,37 @@
+//
+//  SwiftUICoreData.swift
+//  ConinuedLearningUI
+//
+//  Created by Tirzaan on 7/29/25.
+//
+
+import SwiftUI
+import CoreData
+
+class CoreDataViewModel: ObservableObject {
+    
+    let container: NSPersistentContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("ERROR LOADING CORE DATA \(error)")
+            }
+        }
+    }
+    
+}
+
+struct SwiftUICoreData: View {
+    
+    @StateObject var viewModel = CoreDataViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUICoreData()
+}
