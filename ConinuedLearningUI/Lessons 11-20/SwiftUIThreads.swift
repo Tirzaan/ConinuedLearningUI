1a5acc9058813aa9f3bf1aedc01735505c5cd3e1
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-05 14:30:02 -0600

    [Feature] Added Threads UI

--- ConinuedLearningUI/Lessons 11-20/SwiftUIThreads.swift
+++ ConinuedLearningUI/Lessons 11-20/SwiftUIThreads.swift
@@ -8,9 +8,59 @@
 
 import SwiftUI
 
+class ThreadsViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("Is on main thread 1: \(Thread.isMainThread)")
+            print("Current thread 1: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                
+                print("Is on main thread 2: \(Thread.isMainThread)")
+                print("Current thread 2: \(Thread.current)")
+            }
+        }
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+    
+}
+
 struct SwiftUIThreads: View {
+    
+    @StateObject var viewModel = ThreadsViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("Load Data")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        viewModel.fetchData()
+                    }
+                
+                ForEach(viewModel.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundStyle(.red)
+                }
+            }
+        }
     }
 }
 
