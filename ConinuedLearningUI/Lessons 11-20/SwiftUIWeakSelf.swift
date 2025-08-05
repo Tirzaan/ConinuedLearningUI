eef75aef3a04d583852adf438bf658dbafcd82ec
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-05 16:14:12 -0600

    [Feature] Added Weak Self UI

--- ConinuedLearningUI/Lessons 11-20/SwiftUIWeakSelf.swift
+++ ConinuedLearningUI/Lessons 11-20/SwiftUIWeakSelf.swift
@@ -9,9 +9,105 @@
 import SwiftUI
 
 struct SwiftUIWeakSelf: View {
+    
+    @AppStorage("count") var count: Int?
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            ZStack {
+                NavigationLink("Navigate") {
+                    WeakSelfSecondScreen()
+                }
+                
+                
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        HStack {
+                            Text("Scenes Loaded: ")
+                                .font(.headline)
+                            Text("\(count ?? 0)")
+                                .font(.title)
+                        }
+                        .padding()
+                        .background(.green)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    Spacer()
+                    
+                    Text("Reset Count")
+                        .onTapGesture {
+                            count = 0
+                        }
+                }
+                
+            }
+            .navigationTitle(Text("First View"))
+        }
     }
+}
+
+struct WeakSelfSecondScreen: View {
+    
+    @StateObject var viewModel = WeakSelfSecondScreenViewModel()
+    @AppStorage("count") var count: Int?
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Second View")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.red)
+                
+                if let data = viewModel.data {
+                    Text(data)
+                }
+            }
+            VStack {
+                HStack {
+                    Spacer()
+                    HStack {
+                        Text("Scenes Loaded: ")
+                            .font(.headline)
+                        Text("\(count ?? 0)")
+                            .font(.title)
+                    }
+                    .padding()
+                    .background(.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+class WeakSelfSecondScreenViewModel: ObservableObject {
+    
+    @Published var data: String? = nil
+    
+    init() {
+        print("Initialized Now")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        GetData()
+    }
+    
+    deinit {
+        print("Deinitialized Now")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func GetData() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [weak self] in
+            self?.data = "new data"
+        }
+    }
+    
 }
 
 #Preview {
