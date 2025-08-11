abc311469f5c30beaf72a22a4ebc54e5bc3f75c5
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-11 13:01:23 -0600

    [Feature] Added Downloading With Escaping UI

--- ConinuedLearningUI/Lessons 11-20/SwiftUIDownloadWithEscaping.swift
+++ ConinuedLearningUI/Lessons 11-20/SwiftUIDownloadWithEscaping.swift
@@ -8,9 +8,72 @@
 
 import SwiftUI
 
+struct postModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithEscapingViewModel: ObservableObject {
+    
+    @Published var posts: [postModel] = []
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        downloadData(fromURL: url) { returnedData in
+            if let data = returnedData {
+                guard let newPosts = try? JSONDecoder().decode([postModel].self, from: data) else { return }
+                DispatchQueue.main.async { [weak self] in
+                    self?.posts = newPosts
+                }
+            } else {
+                print("No Data Returned!")
+            }
+        }
+        
+    }
+    
+    func downloadData(fromURL url: URL, completionHandler: @escaping (_ data: Data?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                print("Error Downloading data!")
+                completionHandler(nil)
+                return
+            }
+            
+            completionHandler(data)
+            
+        }.resume()
+    }
+    
+}
+
 struct SwiftUIDownloadWithEscaping: View {
+    
+    @StateObject var viewModel = DownloadWithEscapingViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            ForEach(viewModel.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundStyle(.gray)
+                }
+            }
+        }
     }
 }
 
