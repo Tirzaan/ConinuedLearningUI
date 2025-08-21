bcd06f22ec93c4a6864886868f4d7ed9b30ddca9
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-21 13:32:43 -0600

    [Feature] Added Accessibility Colors UI

--- ConinuedLearningUI/Lessons 21-30/SwiftUIAccessibilityColors.swift
+++ ConinuedLearningUI/Lessons 21-30/SwiftUIAccessibilityColors.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct SwiftUIAccessibilityColors: View {
+    
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    @Environment(\.colorSchemeContrast) var colorSchemeContrast
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityInvertColors) var invertColors
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            VStack {
+                Button("Button1") {
+                    
+                }
+                .foregroundStyle(colorSchemeContrast == .increased ? .white : .primary)
+                .buttonStyle(.borderedProminent)
+                
+                Button("Button2") {
+                    
+                }
+                .foregroundStyle(.primary)
+                .buttonStyle(.borderedProminent)
+                .tint(.orange)
+                
+                Button("Button3") {
+                    
+                }
+                .foregroundStyle(invertColors ? .black : .white)
+                .foregroundStyle(.primary)
+                .buttonStyle(.borderedProminent)
+                .tint(invertColors ? .blue : .green)
+                
+                Button("Button4") {
+                    
+                }
+                .foregroundStyle(differentiateWithoutColor ? .white : .green)
+                .foregroundStyle(.primary)
+                .buttonStyle(.borderedProminent)
+                .tint(differentiateWithoutColor ? .black : .purple)
+            }
+            .font(.largeTitle)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(reduceTransparency == true ? .black : .black.opacity(0.5))
+            
+//            .navigationTitle("Hi")
+        }
     }
 }
 
