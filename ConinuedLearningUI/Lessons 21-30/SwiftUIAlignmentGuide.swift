7703c3b9bdc7fa75b2fb71468cb3d941f06cce1a
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-23 10:45:49 -0600

    [Feature] Created Alignment Guide

---
+++ ConinuedLearningUI/Lessons 21-30/SwiftUIAlignmentGuide.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIAlignmentGuide.swift
+//  ConinuedLearningUI
+//
+//  Created by Tirzaan on 8/23/25.
+//
+
+import SwiftUI
+
+struct SwiftUIAlignmentGuide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIAlignmentGuide()
+}
