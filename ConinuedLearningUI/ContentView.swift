586096ab8be871e32d9a609e457ca7fe2de8070f
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-23 10:46:11 -0600

    [Feature] Added Alignment Guide to content view

--- ConinuedLearningUI/ContentView.swift
+++ ConinuedLearningUI/ContentView.swift
@@ -41,7 +41,7 @@ struct ContentView: View {
                 NavigationLink("27 - Accessibility Text") { SwiftUIAccessibilityText() }
                 NavigationLink("28 - Accessibility Colors") { SwiftUIAccessibilityColors() }
                 NavigationLink("29 - Accessibility Voice Over") { SwiftUIAccessibilityVoiceOver() }
-                NavigationLink("30 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
+                NavigationLink("30 - Alignment Guide") { SwiftUIAlignmentGuide() }
                 NavigationLink("31 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
                 NavigationLink("32 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
                 NavigationLink("33 - Nothing") { ZStack {Color.red.ignoresSafeArea(); Text("Lesson not watched yet")} }
