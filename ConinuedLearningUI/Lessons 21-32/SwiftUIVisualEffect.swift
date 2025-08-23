c49314e1600a97c914d6f69b34d54b789d82bc3a
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-23 11:37:22 -0600

    [Feature] Added Visual Effect UI

--- ConinuedLearningUI/Lessons 21-32/SwiftUIVisualEffect.swift
+++ ConinuedLearningUI/Lessons 21-32/SwiftUIVisualEffect.swift
@@ -9,8 +9,43 @@
 import SwiftUI
 
 struct SwiftUIVisualEffect: View {
+    
+//    @State var showSpacer: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            VStack(spacing: 30) {
+                ForEach(0..<100) { index in
+                    Rectangle()
+                        .frame(width: 300, height: 200)
+                        .frame(maxWidth: .infinity)
+                        .background(.orange)
+                        .visualEffect { content, geometry in
+                            content
+                                .offset(x: geometry.frame(in: .global).minY * 0.2)
+                        }
+                }
+            }
+        }
+        
+//        VStack {
+//            Text("Hello, World!")
+//                .padding()
+//                .background(.red)
+//                .visualEffect { content, geometry in
+//                    content
+//                        .grayscale(geometry.frame(in: .global).minY < 300 ? 1 : 0)
+//                    //                    .grayscale(geometry.size.width >= 200 ? 1 : 0)
+//                }
+//            
+//            if showSpacer {
+//                Spacer()
+//            }
+//        }
+//        .animation(.easeInOut, value: showSpacer)
+//        .onTapGesture {
+//            showSpacer.toggle()
+//        }
     }
 }
 
